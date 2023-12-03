222875139eec3f66e974632849696a35370eb407
Spinola_Jack <77943700+1001-Spinola-Jack@users.noreply.github.com>
2023-12-03 10:29:27 -0800

    first commit?
    first commit

---
+++ VRcade Suite/Assets/SettingsMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class settingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void languageMenu()
+    {
+        
+    }
+
+    public void volumeMenu()
+    {
+        
+    }
+
+    public void switchInputfunct()
+    {
+
+    }
+
+
+}
