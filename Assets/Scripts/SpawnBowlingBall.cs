e8cef13f96d597f8fe2585efcb3c67544bf4d0b8
Austin Hendricks <austinhendricks@nevada.unr.edu>
2024-01-24 15:41:46 -0800

    Created script SpawnBowlingBall. Spawns a bowling ball if thumb is extended. When the thumb is reset and extended again, another bowling ball is instantiated.

--- Assets/Scripts/SpawnBowlingBall.cs
+++ Assets/Scripts/SpawnBowlingBall.cs
@@ -1,24 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap;
+using Leap.Unity;
+using static Leap.Unity.Detector;
 
 public class SpawnBowlingBall : MonoBehaviour
 {
-    public GameObject bowlingBall;
+    public GameObject ball;
+    public LeapProvider leapProvider;
+    public bool isInstantiated;
+    public int extCount;
+//    public Hand leftHand;
+//    public Finger thumb;
+//    public Finger.FingerType fingerType;
+//    public PointingType pointingType;
+    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void OnEnable()
     {
-        Vector3 ballPosition = new Vector3(0.482f, 0.711f, 19.959f);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(bowlingBall, ballPosition, Quaternion.identity);
-            // Quaternion.Euler(158.4f, 2.020f, -85.28f
-        }
-        
+        leapProvider.OnUpdateFrame += OnUpdateFrame;
     }
+    private void OnDisable()
+    {
+        leapProvider.OnUpdateFrame -= OnUpdateFrame;
+    }
+    
+    void OnUpdateFrame(Frame frame)
+    {
+        //Use a helpful utility function to get the first hand that matches the Chirality
+        Hand _leftHand = frame.GetHand(Chirality.Left);
+
+        //When we have a valid left hand, we can begin searching for more Hand information
+        if(_leftHand != null)
+        {
+            OnUpdateHand(_leftHand);
+        }
+    }
+
+    void OnUpdateHand(Hand _hand)
+    {
+        extCount = 0;
+        Finger _thumb = _hand.GetThumb();
+        Vector3 ballPosition = new Vector3(0.482f, 0.711f, 19.959f);
+        // if (fingerType == Finger.FingerType.TYPE_THUMB && pointingType == PointingType.RelativeToWorld)
+        if (_thumb.IsExtended && !isInstantiated)
+        {
+            Instantiate(ball, ballPosition, Quaternion.identity);
+            isInstantiated = true;
+        }
+
+        if (!_thumb.IsExtended)
+        {
+            isInstantiated = false;
+        }
+    }
+    
 }
