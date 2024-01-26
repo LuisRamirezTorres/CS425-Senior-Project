ca4fab35d078c2a315468d3b3bd12566a5c6189c
Austin Hendricks <austinhendricks@nevada.unr.edu>
2024-01-25 20:11:39 -0800

    Wrote script PinCollision and test script BallForward. Updated script SpawnBowlingBall.
    Implemented test script BallForward to easily launch a bowling ball forward to collide with pins, testing new script PinCollision which destroys the pins that fall over and the ball after 3 seconds.
    
    SpawnBowlingBall was updated as well which handles respawning of both the bowling ball and the pins. Will be renamed in the future to reflect this.
    
    If the player does a left thumbs up, a bowling ball respawns in front of them, but only one ball can exist at once. This negates any latency that may accidentally allow for a second ball to be spawned if the camera detects a second thumbs up.
    
    Then, there's a check to see if the pins should be reset. This check is if throwCount == 2 and ball.position == pins.position.
    throwCount is incremented when SpawnBall() is called and is reset to 0 when SpawnPins() is called.

--- Assets/Scripts/SpawnBowlingBall.cs
+++ Assets/Scripts/SpawnBowlingBall.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,18 @@ using Leap;
 using Leap.Unity;
 using static Leap.Unity.Detector;
 
+// Template taken from Ultraleap 
+// https://docs.ultraleap.com/xr-and-tabletop/xr/unity/plugin/features/scripting-fundamentals.html
 public class SpawnBowlingBall : MonoBehaviour
 {
     public GameObject ball;
+    public GameObject pins;
+    public Vector3 ballPos;
+    public Vector3 pinPos;
     public LeapProvider leapProvider;
     public bool isInstantiated;
-    public int extCount;
+    public int throwCount;
+    
 //    public Hand leftHand;
 //    public Finger thumb;
 //    public Finger.FingerType fingerType;
@@ -18,9 +25,22 @@ public class SpawnBowlingBall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        throwCount = 0;
+        ballPos = ball.transform.position;  // Get current ball pos
+        pinPos = pins.transform.position;   // Get current pins pos
     }
-    
+
+    void Update()
+    {
+        // If the ball has reached the end of the lane (pinPos) and has been thrown twice
+        // Respawn the pins
+        if (ballPos == pinPos && throwCount == 2)
+        {
+            SpawnPins();
+            throwCount = 0;
+        }
+    }
+
     private void OnEnable()
     {
         leapProvider.OnUpdateFrame += OnUpdateFrame;
@@ -44,13 +64,15 @@ public class SpawnBowlingBall : MonoBehaviour
 
     void OnUpdateHand(Hand _hand)
     {
-        extCount = 0;
         Finger _thumb = _hand.GetThumb();
-        Vector3 ballPosition = new Vector3(0.482f, 0.711f, 19.959f);
+ //       Vector3 ballPosition = new Vector3(0.482f, 0.711f, 19.959f);
         // if (fingerType == Finger.FingerType.TYPE_THUMB && pointingType == PointingType.RelativeToWorld)
-        if (_thumb.IsExtended && !isInstantiated)
+        
+        // To respawn a ball, the left thumb must be extended, then unextended.
+        // The current ball must also not exist
+        if (_thumb.IsExtended && !isInstantiated && !ball)
         {
-            Instantiate(ball, ballPosition, Quaternion.identity);
+            SpawnBall();
             isInstantiated = true;
         }
 
@@ -58,6 +80,18 @@ public class SpawnBowlingBall : MonoBehaviour
         {
             isInstantiated = false;
         }
+    }
+
+    public void SpawnBall()
+    {
+        Instantiate(ball, ballPos, Quaternion.identity);
+        throwCount++;
+    }
+
+    public void SpawnPins()
+    {
+        Instantiate(pins, pinPos, Quaternion.identity);
+        throwCount = 0;
     }
     
 }
