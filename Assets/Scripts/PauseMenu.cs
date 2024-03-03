b8090685d1391582d3ad88cc0e06035904dfd235
Spinola_Jack <77943700+1001-Spinola-Jack@users.noreply.github.com>
2024-03-03 00:27:41 -0800

    -m updated menu, includes a pause menu that stops the in game time and allows users to resume or head back to the game selection menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
+    // Update is called once per frame
+
+    void Start()
+    {
+
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void goToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+
+    }
+    
+    public void quitApplication()
+    {
+        Application.Quit();
+    }
+}
+
+
