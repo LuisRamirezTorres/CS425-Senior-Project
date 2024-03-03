ee6fe2c0e87aa3e69a9bd312fe496f8d4282f0f0
Austin Hendricks <austinhendricks@nevada.unr.edu>
2024-03-03 13:16:57 -0800

    Added script HideCanvas. Deleted old script SpawnDart. Added a canvas that shows a pop up with instructions.
    At start of scene, a pop up will appear explaining the how the user can interact with the game. HideCanvas destroys this Canvas object after 5 seconds, allowing the user to play the game.

---
+++ Assets/Scripts/HideCanvas.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCanvas : MonoBehaviour
+{
+    public GameObject canvasObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DisableCanvas(canvasObject, 5.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DisableCanvas(GameObject canvasObj, float t)
+    {
+        yield return new WaitForSeconds(t);
+        Destroy(canvasObject);
+    }
+}
