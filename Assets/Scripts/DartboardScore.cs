4068237ad0d9ef919577c518a596252a0964f6c3
Austin Hendricks <austinhendricks@nevada.unr.edu>
2024-03-14 15:13:31 -0700

    Working on DartboardScore.cs, right now, 11 is set as the 0 degree point when it should be 6

--- Assets/Scripts/DartboardScore.cs
+++ Assets/Scripts/DartboardScore.cs
@@ -1,30 +1,50 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DartboardScore : MonoBehaviour
 {
     public GameObject dartBoard;
     public GameObject dartTip;
+    public TMP_Text scoreText;
+    public string scoreStr = "Score: ";
 
-    private int score;
-    
+    private int currentScore;
     private Vector3 dartPos;
-    
-    private SphereCollider dartBoardCollider;
-    
+    private Mesh mesh;
+    Bounds bounds;
     private float colliderRadius;
-    private float angle;
-    
     private Vector3 colliderCenter;
+    private float angle;
+
+    public MeshCollider meshCollider;
+    
+//    public SphereCollider dartBoardCollider;
+//    private float colliderRadius;
+//    private Vector3 colliderCenter;
     
     // Start is called before the first frame update
     void Start()
     {
-        dartBoardCollider = GetComponent<SphereCollider>();
+        currentScore = 0;
+        scoreText.text = scoreStr + currentScore.ToString();
+
+        meshCollider = GetComponent<MeshCollider>();
+        colliderCenter = meshCollider.bounds.center;
+        colliderRadius = meshCollider.bounds.extents.y;
+        
+        Debug.Log("colliderCenter: " + colliderCenter);
+        Debug.Log("colliderRadius: " + colliderRadius);
+//        mesh = GetComponent<MeshFilter>().mesh;
+//        bounds = mesh.bounds;
+//        colliderRadius = bounds.extents.x;
+//        colliderCenter = bounds.center;
+
+        /*dartBoardCollider = GetComponent<SphereCollider>();
         colliderRadius = GetComponent<SphereCollider>().radius;
-        colliderCenter = GetComponent<SphereCollider>().center;
+        colliderCenter = GetComponent<SphereCollider>().center;*/
     }
 
     // Update is called once per frame
@@ -35,80 +55,100 @@ public class DartboardScore : MonoBehaviour
 
     private void OnTriggerEnter(Collider collider)
     {
+        Debug.Log("Collision detected");
+        var distanceScore = 0;
+        var angleScore = 0;
         dartPos = dartTip.transform.position;
-        float dartPosX = dartPos.x;
+        Debug.Log("dartPos: " + dartPos);
         float dartPosY = dartPos.y;
-        CheckDistance(dartPosX, dartPosY);
-        angle = GetDartAngle(dartPosX, dartPosY);
-        CheckAngle();
-
+        float dartPosZ = dartPos.z;
+        float dartPosX = dartPos.x;
+        distanceScore = CheckDistance(dartPosY, dartPosZ);
+        angle = GetDartAngle(dartPosY, dartPosZ);
+        Debug.Log("Angle: " + angle);
+        angleScore = CheckAngle();
+        currentScore += (distanceScore * angleScore);
+        scoreText.text = scoreStr + currentScore.ToString();
         
     }
 
-    private float GetDartAngle(float dartPosX, float dartPosY)
+    private float GetDartAngle(float dartPosY, float dartPosZ)
     {
-        return Mathf.Atan2(dartPosY - colliderCenter.y, dartPosX - colliderCenter.x) * 180 / Mathf.PI;
+        float degrees = Mathf.Atan2(dartPosY - colliderCenter.y, dartPosZ - colliderCenter.z) * 180 / Mathf.PI;
+    //    degrees = (degrees + 360) % 360;
+    //    return degrees;
+        if (degrees > 90)
+            degrees = 450 - degrees;
+        else
+            degrees = 90 - degrees;
+        return degrees;
     }
 
-    private void CheckDistance(float dartPosX, float dartPosY)
+    private int CheckDistance(float dartPosY, float dartPosZ)
     {
-        if (dartPosY == (0.5f * colliderRadius) || (dartPosX == (0.5f * colliderRadius)))
+        var distanceScore = 1;
+        if (dartPosY == (0.5f * colliderRadius) || (dartPosZ == (0.5f * colliderRadius)))
         {
-            score *= 3;
+            distanceScore = 3;
         }
         
-        if (dartPosY == colliderRadius || dartPosX == 0.5f * colliderRadius)
+        if (dartPosY == colliderRadius || dartPosZ == 0.5f * colliderRadius)
         {
-            score *= 2;
+            distanceScore = 2;
         }
+        Debug.Log("distanceScore: " + distanceScore);
+        return distanceScore;
     }
 
-    private void CheckAngle()
+    private int CheckAngle()
     {
+        var angleScore = 0;
         if (angle is < 81.0f and > 63.0f)
-            score += 1;
+            angleScore = 1;
         if (angle is < 63.0f and > 45.0f)
-            score += 18;
+            angleScore = 18;
         if (angle is < 45.0f and > 27.0f)
-            score += 4;
+            angleScore = 4;
         if (angle is < 27.0f and > 9.0f)
-            score += 13;
+            angleScore = 13;
         
         if (angle is < 351.0f and > 9.0f)
-            score += 6;
+            angleScore = 6;
         if (angle is < 351.0f and > 333.0f)
-            score += 10;
+            angleScore = 10;
         if (angle is < 333.0f and > 315.0f)
-            score += 15;
+            angleScore = 15;
         if (angle is < 315.0f and > 297.0f)
-            score += 2;
+            angleScore = 2;
         
         if (angle is < 297.0f and > 279.0f)
-            score += 17;
+            angleScore = 17;
         if (angle is < 279.0f and > 261.0f)
-            score += 3;
+            angleScore = 3;
         if (angle is < 261.0f and > 243.0f)
-            score += 19;
+            angleScore = 19;
         if (angle is < 243.0f and > 225.0f)
-            score += 7;
+            angleScore = 7;
         
         if (angle is < 225.0f and > 207.0f)
-            score += 16;
+            angleScore = 16;
         if (angle is < 207.0f and > 189.0f)
-            score += 8;
+            angleScore = 8;
         if (angle is < 189.0f and > 171.0f)
-            score += 11;
+            angleScore = 11;
         if (angle is < 171.0f and > 153.0f)
-            score += 14;
+            angleScore = 14;
         
         if (angle is < 153.0f and > 135.0f)
-            score += 9;
+            angleScore = 9;
         if (angle is < 135.0f and > 117.0f)
-            score += 12;
+            angleScore = 12;
         if (angle is < 117.0f and > 99.0f)
-            score += 5;
+            angleScore = 5;
         if (angle is < 99.0f and > 81.0f)
-            score += 20;
-    
+            angleScore = 20;
+
+        Debug.Log("angleScore: " + angleScore);
+        return angleScore;
     }
 }
