cd2544072ac7d3354822caf8e2445f71c730d5fe
Austin Hendricks <austinhendricks@nevada.unr.edu>
2024-03-14 19:37:31 -0700

    CheckAngle() and GetAngle() in DartboardScore.cs now registers the correct score according to the angle of the Dart and the center of the Dartboard

--- Assets/Scripts/DartboardScore.cs
+++ Assets/Scripts/DartboardScore.cs
@@ -12,7 +12,7 @@ public class DartboardScore : MonoBehaviour
     public string scoreStr = "Score: ";
 
     private int currentScore;
-    private Vector3 dartPos;
+
     private Mesh mesh;
     Bounds bounds;
     private float colliderRadius;
@@ -56,17 +56,17 @@ public class DartboardScore : MonoBehaviour
     private void OnTriggerEnter(Collider collider)
     {
         Debug.Log("Collision detected");
-        var distanceScore = 0;
-        var angleScore = 0;
-        dartPos = dartTip.transform.position;
+
+        Vector3 dartPos = dartTip.transform.position;
         Debug.Log("dartPos: " + dartPos);
         float dartPosY = dartPos.y;
         float dartPosZ = dartPos.z;
-        float dartPosX = dartPos.x;
-        distanceScore = CheckDistance(dartPosY, dartPosZ);
+        
+//        distanceScore = CheckDistance(dartPosY, dartPosZ);
+        var distanceScore = CheckDistance(dartPos);
         angle = GetDartAngle(dartPosY, dartPosZ);
         Debug.Log("Angle: " + angle);
-        angleScore = CheckAngle();
+        var angleScore = CheckAngle();
         currentScore += (distanceScore * angleScore);
         scoreText.text = scoreStr + currentScore.ToString();
         
@@ -75,27 +75,37 @@ public class DartboardScore : MonoBehaviour
     private float GetDartAngle(float dartPosY, float dartPosZ)
     {
         float degrees = Mathf.Atan2(dartPosY - colliderCenter.y, dartPosZ - colliderCenter.z) * 180 / Mathf.PI;
-    //    degrees = (degrees + 360) % 360;
+        degrees = (degrees + 360) % 360;
     //    return degrees;
-        if (degrees > 90)
-            degrees = 450 - degrees;
+        /*if (degrees > 180)
+            degrees = 270 + degrees;
         else
-            degrees = 90 - degrees;
+            degrees = 90 + degrees;*/
         return degrees;
     }
 
-    private int CheckDistance(float dartPosY, float dartPosZ)
+    private int CheckDistance(Vector3 dartPos)
     {
-        var distanceScore = 1;
-        if (dartPosY == (0.5f * colliderRadius) || (dartPosZ == (0.5f * colliderRadius)))
+        var distanceScore = 0;
+        float dartDistanceFromCenter = Vector3.Distance(dartPos, colliderCenter);
+        float halfRadius = colliderRadius * 0.5f;
+
+        if (dartDistanceFromCenter == colliderRadius)
+            distanceScore = 2;
+        else if (dartDistanceFromCenter is < 0.3507f and > 0.3206f)
+            distanceScore = 3;
+        else
+            distanceScore = 1;
+        
+        /*if (dartPosY == (0.5f * colliderRadius) || (dartPosZ == (0.5f * colliderRadius)))
         {
             distanceScore = 3;
         }
         
-        if (dartPosY == colliderRadius || dartPosZ == 0.5f * colliderRadius)
+        if (dartPosY == colliderRadius || dartPosZ == colliderRadius)
         {
             distanceScore = 2;
-        }
+        }*/
         Debug.Log("distanceScore: " + distanceScore);
         return distanceScore;
     }
@@ -103,7 +113,53 @@ public class DartboardScore : MonoBehaviour
     private int CheckAngle()
     {
         var angleScore = 0;
+        
+        if (angle is < 351.0f and > 9.0f)
+            angleScore = 11;
+        if (angle is < 27.0f and > 9.0f)
+            angleScore = 14;
+        if (angle is < 45.0f and > 27.0f)
+            angleScore = 9;
+        if (angle is < 63.0f and > 45.0f)
+            angleScore = 12;
+        
         if (angle is < 81.0f and > 63.0f)
+            angleScore = 5;
+        if (angle is < 99.0f and > 81.0f)
+            angleScore = 20;
+        if (angle is < 117.0f and > 99.0f)
+            angleScore = 1;
+        if (angle is < 135.0f and > 117.0f)
+            angleScore = 18;
+        
+        if (angle is < 153.0f and > 135.0f)
+            angleScore = 4;
+        if (angle is < 171.0f and > 153.0f)
+            angleScore = 13;
+        if (angle is < 189.0f and > 171.0f)
+            angleScore = 6;
+        if (angle is < 207.0f and > 189.0f)
+            angleScore = 10;
+        
+        if (angle is < 225.0f and > 207.0f)
+            angleScore = 15;
+        if (angle is < 243.0f and > 225.0f)
+            angleScore = 2;
+        if (angle is < 261.0f and > 243.0f)
+            angleScore = 17;
+        if (angle is < 279.0f and > 261.0f)
+            angleScore = 3;
+        
+        if (angle is < 297.0f and > 279.0f)
+            angleScore = 19;
+        if (angle is < 315.0f and > 297.0f)
+            angleScore = 7;
+        if (angle is < 333.0f and > 315.0f)
+            angleScore = 16;
+        if (angle is < 351.0f and > 333.0f)
+            angleScore = 8;
+        
+        /*if (angle is < 81.0f and > 63.0f)
             angleScore = 1;
         if (angle is < 63.0f and > 45.0f)
             angleScore = 18;
@@ -146,7 +202,7 @@ public class DartboardScore : MonoBehaviour
         if (angle is < 117.0f and > 99.0f)
             angleScore = 5;
         if (angle is < 99.0f and > 81.0f)
-            angleScore = 20;
+            angleScore = 20;*/
 
         Debug.Log("angleScore: " + angleScore);
         return angleScore;
