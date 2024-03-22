6d2e0acf2c50b9a48a8f907590115f8fdafac350
Spinola_Jack <77943700+1001-Spinola-Jack@users.noreply.github.com>
2024-03-22 12:14:26 -0700

    -m menu sound intit

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [Header("--------------Audio Source--------------")]
+    
+    [SerializeField] AudioSource musicSource;
+
+    [Header("--------------Audio Clip--------------")]
+
+    public AudioClip background;
+
+}
