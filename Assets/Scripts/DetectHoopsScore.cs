13e616d0feb136a025a728b5468d896987933af0
Emanuel Gutierrez <emanuelgtz02@gmail.com>
2024-03-30 17:30:16 -0700

    Imroved hoops score and timer to be more in theme with arcade, Fixed issue of ball getting stuck in the net

--- Assets/Scripts/DetectHoopsScore.cs
+++ Assets/Scripts/DetectHoopsScore.cs
@@ -8,7 +8,10 @@ public class DetectHoopsScore : MonoBehaviour
 {
     private int score = 0;
     private float time_remaining = 60.0f;
-    public TextMeshPro score_text;
+    private float disable_time = 0.25f;
+    private bool collider_disabled = false;
+    public TextMeshProUGUI score_text;
+    public GameObject basketballMachine;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +23,28 @@ public class DetectHoopsScore : MonoBehaviour
     {
         time_remaining -= Time.deltaTime;
         score_text.text = "Score: " + score.ToString() + "\n" + "Time: " + Math.Max(Math.Floor(time_remaining), 0).ToString();
-
+        if (collider_disabled && disable_time > 0)
+        {
+            disable_time -= Time.deltaTime;
+        }
+        else if (collider_disabled && disable_time <= 0)
+        {
+            collider_disabled = false;
+            basketballMachine.GetComponent<MeshCollider>().enabled = true;
+            disable_time = 0.25f;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collision detected");
         print(other.tag);
-        Destroy(other.gameObject);
+        //Destroy(other.gameObject);
         if (time_remaining > 0)
         {
             score++;
         }
+        basketballMachine.GetComponent<MeshCollider>().enabled = false;
+        collider_disabled = true;
     }
     void restart()
     {
