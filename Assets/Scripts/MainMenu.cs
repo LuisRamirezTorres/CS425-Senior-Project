c9b512c68c1e91ab498dffd80638dfde3cbada05
Spinola_Jack <77943700+1001-Spinola-Jack@users.noreply.github.com>
2024-04-10 21:41:58 -0700

    -m updated menu UI, displayable audio slider

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void enterSelection()
+    {
+        SceneManager.LoadScene("GameSelection");
+    }
+
+    public void enterSettings()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}
