379a8dc9023753298bbb1f21346a08471fcc18df
Spinola_Jack <77943700+1001-Spinola-Jack@users.noreply.github.com>
2024-04-21 14:58:49 -0700

    -m leap menu integrated

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,161 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap;
+using Leap.Unity;
+using static Leap.Unity.Detector;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
+
+public class MainMenu : MonoBehaviour { 
+
+    public LeapProvider leapProvider;
+
     // Start is called before the first frame update
+
+    private bool extendCheck;
+
+    private bool isPaused = true; // Initially pause the script
+    private WaitForSeconds delay = new WaitForSeconds(5f);
+
+
+
+    void Start()
+    {
+        StartCoroutine(StartAfterDelay());
+    }
+
+    IEnumerator StartAfterDelay()
+    {
+        yield return delay; // Wait for 5 seconds
+
+        // Now the script execution starts
+        isPaused = false;
+
+        // Your script logic here
+    }
+
+
+
+
+
+
+
+
+
+
+    private void OnEnable()
+    {
+        leapProvider.OnUpdateFrame += OnUpdateFrame;
+    }
+    private void OnDisable()
+    {
+        leapProvider.OnUpdateFrame -= OnUpdateFrame;
+    }
+
+    void OnUpdateFrame(Frame frame)
+    {
+        // Use a helpful utility function to get the first hand that matches the Chirality
+        Hand _leftHand = frame.GetHand(Chirality.Left);
+        Hand _rightHand = frame.GetHand(Chirality.Right);
+
+        // When we have a valid left hand, we can begin searching for more Hand information
+        if (_leftHand != null /*|| _rightHand != null*/)
+        {
+            OnUpdateHand(_leftHand,"left");
+           // OnUpdateHand(_rightHand);
+        }
+
+        if(_rightHand != null)
+        {
+           
+            OnUpdateHand(_rightHand, "right");
+        }
+    }
+
+    void OnUpdateHand(Hand _hand, string side)
+    {
+
+
+
+        if (IsExtended(_hand) && !extendCheck && side == "right")
+        {
+            Debug.Log("All fingers are extended");
+            enterSelection();
+            extendCheck = true;
+        }
+
+
+        if (IsThumbsUp(_hand) && !extendCheck && side == "left")
+        {
+            Debug.Log("Thumb is up");
+            enterSettings();
+            extendCheck = true;
+        }
+
+        if (!IsExtended(_hand))
+        {
+            extendCheck = false;
+        }
+
+        if (!IsThumbsUp(_hand))
+        {
+
+            extendCheck = false;
+        }
+    }
+
+    bool IsExtended(Hand _hand)
+    {
+        bool isHandOpen;
+        Finger _thumb = _hand.GetThumb();
+        Finger _index = _hand.GetIndex();
+        Finger _middle = _hand.GetMiddle();
+        Finger _ring = _hand.GetRing();
+        Finger _pinky = _hand.GetPinky();
+
+        bool isThumb = _thumb.IsExtended;
+        bool isIndex = _index.IsExtended;
+        bool isMiddle = _middle.IsExtended;
+        bool isRing = _ring.IsExtended;
+        bool isPinky = _pinky.IsExtended;
+
+        if (isThumb && isIndex && isMiddle && isRing && isPinky)
+            isHandOpen = true;
+        else
+            isHandOpen = false;
+
+        return isHandOpen;
+    }
+
+
+    bool IsThumbsUp(Hand _hand)
+    {
+        bool isThumbsUp;
+
+        Finger _thumb = _hand.GetThumb();
+        Finger _index = _hand.GetIndex();
+        Finger _middle = _hand.GetMiddle();
+        Finger _ring = _hand.GetRing();
+        Finger _pinky = _hand.GetPinky();
+
+
+        bool isThumb = _thumb.IsExtended;
+        bool isIndex = _index.IsExtended;
+        bool isMiddle = _middle.IsExtended;
+        bool isRing = _ring.IsExtended;
+        bool isPinky = _pinky.IsExtended;
+
+        if (isThumb)
+            isThumbsUp = true;
+        else
+            isThumbsUp = false;
+
+        return isThumbsUp;
+    }
+
+
     public void enterSelection()
     {
         SceneManager.LoadScene("GameSelection");
--- Assets/Scripts/gameSelector.cs
+++ Assets/Scripts/gameSelector.cs
@@ -1,28 +1,326 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap;
+using Leap.Unity;
+using static Leap.Unity.Detector;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
+
 public class gameSelector : MonoBehaviour
 {
-    
-  
+    public GameObject skeeBallPanel;
+    public GameObject dartsPanel;
+    public GameObject basketBallPanel;
+
     public static int gameCountReference;
 
-    private AudioManager audioManager; 
+    private AudioManager audioManager;
+
+
+    public LeapProvider leapProvider;
+
+    // Start is called before the first frame update
+
+    private bool extendCheck;
 
     void Start()
     {
-        
-        
+        basketBallPanel.SetActive(true);
+        skeeBallPanel.SetActive(false);
+        dartsPanel.SetActive(false);
+
         gameCountReference = 0;
 
         // Find AudioManager instance in the scene
         audioManager = FindObjectOfType<AudioManager>();
+
+
+        StartCoroutine(MyCoroutine());
     }
 
-    public void chooseGame()
+
+    IEnumerator MyCoroutine()
+    {
+        Debug.Log("Coroutine started");
+
+        yield return new WaitForSeconds(2); // Wait for 2 seconds
+
+        Debug.Log("Coroutine resumed after waiting for 2 seconds");
+    }
+
+
+
+
+
+    private void OnEnable()
+    {
+        leapProvider.OnUpdateFrame += OnUpdateFrame;
+    }
+    private void OnDisable()
+    {
+        leapProvider.OnUpdateFrame -= OnUpdateFrame;
+    }
+
+    void OnUpdateFrame(Frame frame)
+    {
+        // Use a helpful utility function to get the first hand that matches the Chirality
+        Hand _leftHand = frame.GetHand(Chirality.Left);
+        Hand _rightHand = frame.GetHand(Chirality.Right);
+
+        // When we have a valid left hand, we can begin searching for more Hand information
+        if (_leftHand != null )
+        {
+            OnUpdateHand(_leftHand, "left");
+           
+        }
+
+        if (_rightHand != null)
+        {
+
+            OnUpdateHand(_rightHand, "right");
+        }
+    }
+
+    void OnUpdateHand(Hand _hand, string side)
+    {
+
+
+
+        if (IsExtended(_hand) && !extendCheck && side == "left")
+        {
+            Debug.Log("All fingers are extended");
+            goToMenu();
+            extendCheck = true;
+        }
+
+
+        if (IsThumbsUp(_hand) && !extendCheck && side == "right")
+        {
+            Debug.Log("Thumb is up");
+            playGame();
+            extendCheck = true;
+        }
+
+        if (isPointingLeft(_hand) && !extendCheck)
+        {
+            Debug.Log("Finger is pointing left");
+            previousGame();
+            extendCheck = true;
+            StartCoroutine(MyCoroutine());
+
+        }
+
+        if (isPointingRight(_hand) && !extendCheck)
+        {
+            Debug.Log("Finger is pointing right");
+            nextGame();
+            extendCheck = true;
+            StartCoroutine(MyCoroutine());
+        }
+
+
+
+        if (!IsExtended(_hand))
+        {
+            extendCheck = false;
+        }
+
+        if (!IsThumbsUp(_hand))
+        {
+
+            extendCheck = false;
+        }
+
+        if (!isPointingLeft(_hand))
+        {
+            extendCheck = false;
+        }
+
+
+
+    }
+
+    bool IsExtended(Hand _hand)
+    {
+        bool isHandOpen;
+        Finger _thumb = _hand.GetThumb();
+        Finger _index = _hand.GetIndex();
+        Finger _middle = _hand.GetMiddle();
+        Finger _ring = _hand.GetRing();
+        Finger _pinky = _hand.GetPinky();
+
+        bool isThumb = _thumb.IsExtended;
+        bool isIndex = _index.IsExtended;
+        bool isMiddle = _middle.IsExtended;
+        bool isRing = _ring.IsExtended;
+        bool isPinky = _pinky.IsExtended;
+
+        if (isThumb && isIndex && isMiddle && isRing && isPinky)
+            isHandOpen = true;
+        else
+            isHandOpen = false;
+
+        return isHandOpen;
+    }
+
+    bool isPointingLeft(Hand _hand)
+    {
+        bool isFingerLeft;
+        Finger _thumb = _hand.GetThumb();
+        Finger _index = _hand.GetIndex();
+        Finger _middle = _hand.GetMiddle();
+        Finger _ring = _hand.GetRing();
+        Finger _pinky = _hand.GetPinky();
+
+        bool isThumb = _thumb.IsExtended;
+        bool isIndex = _index.IsExtended;
+        bool isMiddle = _middle.IsExtended;
+        bool isRing = _ring.IsExtended;
+        bool isPinky = _pinky.IsExtended;
+        
+        
+        Vector3 indexFingerDirection = _index.Direction;
+       
+        //Debug.Log("Index Finger Direction: " + indexFingerDirection);
+        
+
+
+
+        if (isIndex && indexFingerDirection.x < -.85)
+        {
+            isFingerLeft = true;
+        }
+        else
+        {
+            isFingerLeft = false;
+        }
+        return isFingerLeft;
+       
+
+    }
+
+    bool isPointingRight(Hand _hand)
+    {
+        bool isFingerLeft;
+        Finger _thumb = _hand.GetThumb();
+        Finger _index = _hand.GetIndex();
+        Finger _middle = _hand.GetMiddle();
+        Finger _ring = _hand.GetRing();
+        Finger _pinky = _hand.GetPinky();
+
+        bool isThumb = _thumb.IsExtended;
+        bool isIndex = _index.IsExtended;
+        bool isMiddle = _middle.IsExtended;
+        bool isRing = _ring.IsExtended;
+        bool isPinky = _pinky.IsExtended;
+
+
+        Vector3 indexFingerDirection = _index.Direction;
+        
+       
+
+       // Debug.Log("Index Finger Direction: " + indexFingerDirection);
+
+
+
+
+        if (isIndex && indexFingerDirection.x > .85)
+        {
+            isFingerLeft = true;
+        }
+        else
+        {
+            isFingerLeft = false;
+        }
+        return isFingerLeft;
+
+
+    }
+
+
+    bool IsThumbsUp(Hand _hand)
+    {
+        bool isThumbsUp;
+
+        Finger _thumb = _hand.GetThumb();
+        Finger _index = _hand.GetIndex();
+        Finger _middle = _hand.GetMiddle();
+        Finger _ring = _hand.GetRing();
+        Finger _pinky = _hand.GetPinky();
+        
+
+
+        bool isThumb = _thumb.IsExtended;
+        bool isIndex = _index.IsExtended;
+        bool isMiddle = _middle.IsExtended;
+        bool isRing = _ring.IsExtended;
+        bool isPinky = _pinky.IsExtended;
+        
+
+        if (isThumb && !isIndex)
+            isThumbsUp = true;
+        else
+            isThumbsUp = false;
+
+        return isThumbsUp;
+    }
+
+
+
+
+
+    public void nextGame()
+    {
+        if (gameCountReference == 0)
+        {
+            skeeBallPanel.SetActive(true);
+            basketBallPanel.SetActive(false);
+            gameCountReference++;
+        }
+        else if (gameCountReference == 1)
+        {
+
+            dartsPanel.SetActive(true);
+            skeeBallPanel.SetActive(false);
+        }
+        else if (gameCountReference == 2)
+        {
+            basketBallPanel.SetActive(true);
+            dartsPanel.SetActive(false);
+            gameCountReference = 0;
+        }
+    }
+
+    public void previousGame()
+    {
+        if (gameCountReference == 0)
+        {
+            basketBallPanel.SetActive(false);
+            dartsPanel.SetActive(true);
+            gameCountReference = 2;
+        }
+        else if (gameCountReference == 1)
+        {
+            skeeBallPanel.SetActive(false);
+            basketBallPanel.SetActive(true);
+            gameCountReference--;
+        }
+        else if (gameCountReference == 2)
+        {
+            dartsPanel.SetActive(false);
+            skeeBallPanel.SetActive(true);
+            gameCountReference--;
+        }
+       
+    }
+
+
+
+
+    /* public void chooseGame()
     {
         string ClickedButtonName = EventSystem.current.currentSelectedGameObject.name;
 
@@ -46,7 +344,7 @@ public class gameSelector : MonoBehaviour
         {
             goToMenu();
         }
-    }
+    }*/
 
     public void playGame()
     {
@@ -75,6 +373,9 @@ public class gameSelector : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+
+    
 
 
 }
