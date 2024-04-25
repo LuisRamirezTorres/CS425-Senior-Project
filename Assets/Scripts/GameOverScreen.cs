5b7d5cbc51a9896e12a1fb62aaa2ffaddcfdf71b
Chito7 <luisrt720@gmail.com>
2024-04-24 19:16:01 -0700

    Game over gestures
    Thumbs up gesture to restart the game implemented.

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GameOverScreen : MonoBehaviour
 
     private GameManager instance;
     public TextMeshProUGUI gameOverText;
+    public SkeeballGestures endGame;
 
     // Start is called before the first frame update
     public void Start()
@@ -20,6 +22,12 @@ public class GameOverScreen : MonoBehaviour
     public void SetUp()
     {
         gameObject.SetActive(true);
+        endGame.Setup();
         gameOverText.text = instance.getScore().ToString() +" POINTS!\n";
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Skeeball");
     }
 }
